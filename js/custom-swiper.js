181bbc250d6c4cb91ccf9194e29edeb982a94e36
Isaac Marshall <isaacmarshall@MacBook-Air-de-Isaac.local>
2024-07-22 11:28:22 +0200

    merge main to dev

---
+++ js/custom-swiper.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        centeredSlides: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },breakpoints: {
+            420: {
+              slidesPerView: 1,
+              spaceBetween: 10,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 40,
+            }
+          },
+      });
+});
