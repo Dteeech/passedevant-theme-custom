3a5391454464f0ab5669558bc9aca5038ef833c3
Dteeech <marshall.isaac@protonmail.com>
2024-08-07 12:10:31 +0200

    debug github

---
+++ src/swiper.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const settings = {
+        loop: true,
+        speed: 700,
+        pagination: {
+            el: ".swiper-pagination",
+            type: "bullets"
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        }
+    };
+
+    const swiper = new Swiper(".myPhilosophieSwiper", settings);
+
+    document.addEventListener('DOMContentLoaded', function() {
+        document.querySelectorAll('.menu a').forEach(function(link) {
+            link.addEventListener('click', function(e) {
+                e.preventDefault();
+                var slideIndex = this.getAttribute('data-slide');
+                swiper.slideTo(slideIndex);
+            });
+        });
+     })
+});
