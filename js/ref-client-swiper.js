891424fdf79785689805f8a42b47136f255dd513
Dteeech <marshall.isaac@protonmail.com>
2024-09-02 15:57:14 +0200

    ajout des fleches au carousel cards de la page references clients

--- js/ref-client-swiper.js
+++ js/ref-client-swiper.js
@@ -2,5 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
     var swiper = new Swiper(".refClientSwiper", {
         effect: "cards",
         grabCursor: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        }
     });
 });
