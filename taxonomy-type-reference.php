709a3df2c1bd69f2423f917fea999e7c6e7fff32
Dteeech <marshall.isaac@protonmail.com>
2024-10-07 11:27:24 +0200

    fix : modification du padding pour les catégories de blog et de remontée clients

--- taxonomy-type-reference.php
+++ taxonomy-type-reference.php
@@ -89,7 +89,7 @@ if ($ref_query->have_posts()) :
     <style>
         .categories {
 
-            margin: 145px 0 45px 0;
+            padding-top: 145px;
         }
 
         .tax-type-reference .category-filter {
