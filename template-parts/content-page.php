5d3908bf110e9fbf36120f9d05171289a29b22b3
Dteeech <marshall.isaac@protonmail.com>
2024-10-08 10:45:27 +0200

    fix: supression de la remontée image mise en avant sur les pages

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying page content in page.php
  *
@@ -11,10 +12,10 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header pb-0 pt-0">
-		<!-- <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?> -->
+		<!-- <?php the_title('<h1 class="entry-title">', '</h1>'); ?> -->
 	</header><!-- .entry-header -->
 
-	<?php passedevant_post_thumbnail(); ?>
+
 
 	<div class="entry-content mt-0">
 		<?php
@@ -22,28 +23,28 @@
 
 		wp_link_pages(
 			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'passedevant' ),
+				'before' => '<div class="page-links">' . esc_html__('Pages:', 'passedevant'),
 				'after'  => '</div>',
 			)
 		);
 		?>
 	</div><!-- .entry-content -->
 
-	<?php if ( get_edit_post_link() ) : ?>
+	<?php if (get_edit_post_link()) : ?>
 		<footer class="entry-footer">
 			<?php
 			edit_post_link(
 				sprintf(
 					wp_kses(
 						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'passedevant' ),
+						__('Edit <span class="screen-reader-text">%s</span>', 'passedevant'),
 						array(
 							'span' => array(
 								'class' => array(),
 							),
 						)
 					),
-					wp_kses_post( get_the_title() )
+					wp_kses_post(get_the_title())
 				),
 				'<span class="edit-link">',
 				'</span>'
