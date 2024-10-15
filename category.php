174cda35ca6785adbc6ffea0507a8768f13f7d3f
Dteeech <marshall.isaac@protonmail.com>
2024-10-15 14:37:05 +0200

    modification du hero du blog

--- category.php
+++ category.php
@@ -9,10 +9,18 @@ get_header(); ?>
 
 
 <main id="primary" class="site-main">
-    <div class="blog-hero">
-        <div class="flex flex-col gap-8 container lg:w-1/2 lg:h-full lg:ms-52 mt-28">
+    <div class="blog-hero flex flex-row justify-center items-center">
+        <div class="flex flex-col container lg:w-1/4 lg:h-full lg:ms-52 mt-28">
             <h1 class="text-5xl text-white">Le Blog de l'actu SEO</h1>
+            <div class=" container text-white flex flex-col gap-4 items-center justify-center">
+                <p class="text-xl">Petit texte à propos du blog</p>
+
+            </div>
+        </div>
+        <div class="flex flex-col container lg:w-1/3 lg:h-full lg:ms-52 mt-28">
+
             <div class="blog-hero_form_container container text-white flex flex-col gap-4">
+
                 <p class=" text-2xl">Newsletter</p>
                 <p class="text-xl">Inscrivez-vous et recevez les dernières actualités digitales directement dans votre boîte mail.</p>
                 <form class="blog-newsletter-form">
